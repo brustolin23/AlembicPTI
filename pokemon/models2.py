85ad991b87b83bc1a10e3527365d8c11ff7dd0d7
brustolin23 <102733510+brustolin23@users.noreply.github.com>
2024-06-17 14:47:05 -0300

    Migração da tabela tipos

--- pokemon/models2.py
+++ pokemon/models2.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Float, Integer, String
+from sqlalchemy import Column, Float, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -10,6 +10,30 @@ class TbPokemon(Base):
     poke_registerDex = Column(Integer, primary_key=True)
     poke_name = Column(String(20), nullable=False)
     pokedex_entry = Column(String(500), nullable=False)
+    poke_primaryType = Column(ForeignKey("tb_type.type_id"))
+    poke_secondaryType = Column(ForeignKey("tb_type.type_id"))
     poke_height = Column(Float, nullable=False)
     poke_weight = Column(Float, nullable=False)
     poke_picture = Column(String(10), nullable=False)
+
+class TbType(Base):
+    __tablename__ = 'tb_type'
+
+    type_id = Column(Integer, primary_key=True)
+    type_name = Column(String(20), nullable=False)
+    type_picture = Column(String(10), nullable=False)
+
+class TbCombatAdvantage(Base):
+    __tablename__ = 'tb_combatAdvantage'
+
+    cmAd_id = Column(Integer, primary_key=True)
+    cmAd_name = Column(String(20), nullable=False)
+    cmAd_multiplier = Column(Float, nullable=False)
+
+class TbTypeAdvantage(Base):
+    __tablename__ = 'tb_typeAdvantage'
+
+    tyAd_id = Column(Integer, primary_key=True)
+    tyAd_attackType = Column(Integer)
+    tyAd_defenseType = Column(Integer)
+    tyAd_typeAdvantage = Column(Integer)
